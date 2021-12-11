e4398ca06db43258467cc1eaf03041aa68a3a35c
Markus Felten <Markus.Felten@gmx.de>
2021-12-11 17:42:53 +0100

    test: prepare for class method

--- tests/helper/util.mjs
+++ tests/helper/util.mjs
@@ -14,23 +14,58 @@ export async function* sequence(name, time = 100, num = 10, errIndex = -1) {
   }
 }
 
-function generateSequences(x) {
-  return Array.isArray(x) ? sequence(...x) : x;
+export class Sequencer {
+  constructor(name, time = 100, num = 5, errIndex = -1) {
+    this.name = name;
+    this.time = time;
+    this.num = num;
+    this.errIndex = errIndex;
+  }
+
+  async *[Symbol.asyncIterator]() {
+    for (let i = 0; i < this.num; i += 1) {
+      yield new Promise((resolve, reject) =>
+        setTimeout(() => {
+          if (i === this.errIndex) {
+            reject(this.name + i);
+          } else {
+            resolve(this.name + i);
+          }
+        }, this.time)
+      );
+    }
+  }
+}
+
+function generateSequences(x, useClass) {
+  return Array.isArray(x) ? useClass ? new Sequencer(...x) : sequence(...x) : x;
 }
 
 export async function aft(t, aggregator, input, expected, failed) {
   const results = [];
 
-  input;
   try {
-    for await (const r of aggregator(input.map(x => generateSequences(x)))) {
+    for await (const r of aggregator(input.map(x => generateSequences(x, false)))) {
       results.push(r);
     }
   } catch (e) {
     t.is(e, failed);
   }
 
-  t.deepEqual(results, expected);
+  t.deepEqual(results, expected, "direct AsyncIterator");
+
+/*
+  results.length = 0;
+  try {
+    for await (const r of aggregator(input.map(x => generateSequences(x, true)))) {
+      results.push(r);
+    }
+  } catch (e) {
+    t.is(e, failed);
+  }
+
+  t.deepEqual(results, expected, "class with AsyncIterator");
+  */
 }
 
 
